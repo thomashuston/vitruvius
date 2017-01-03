63944d79cbdd2346eaa0e7e7feb7e5f5398f20fa
Thomas Huston <tphuston90@gmail.com>
2017-01-02 22:59:00 -0500

    Add a manual clean and build step to install so that lint and test work properly.

---
+++ scripts/build.js
@@ -0,0 +1,18 @@
+import * as path from 'path';
+import buildPackage from '../packages/vitruvius-build-package/src';
+
+const ROOT_DIR = path.resolve(path.join(__dirname, '..'));
+
+// TODO: Replace with vitruvius-lerna package
+const packages = [
+    'vitruvius',
+    'vitruvius-build-package'
+];
+
+packages.forEach((p) => {
+    const packageDir = path.join(ROOT_DIR, 'packages', p);
+    buildPackage({
+        srcDir: path.join(packageDir, 'src'),
+        destDir: path.join(packageDir, 'lib')
+    });
+});
