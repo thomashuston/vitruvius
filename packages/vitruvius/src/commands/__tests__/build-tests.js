27f5779195676c6cde1f655af9065c8010bfac53
Thomas Huston <twelveangrymen435@gmail.com>
2017-03-04 13:16:23 -0500

    Update master build command to run against all detected lerna packages.

--- packages/vitruvius/src/commands/__tests__/build-tests.js
+++ packages/vitruvius/src/commands/__tests__/build-tests.js
@@ -2,16 +2,28 @@ import * as path from 'path';
 import buildPackage from 'vitruvius-build-package';
 import { handler as build } from '../build';
 
+let mockPackages;
+jest.mock('vitruvius-lerna', () => ({
+    getPackages: () => mockPackages
+}));
+
 jest.mock('vitruvius-build-package', () => jest.fn());
 
-it('builds code in the source directory into the destination directory', () => {
+it('builds code in the source directory into the destination directory for all packages', () => {
+    mockPackages = [
+        '/foo/bar/packages/fake-pkg-1',
+        '/foo/bar/packages/fake-pkg-2'
+    ];
+
     build({
         srcDir: 'src',
         destDir: 'lib'
     });
 
-    expect(buildPackage).toHaveBeenCalledWith({
-        srcDir: path.join(process.cwd(), 'src'),
-        destDir: path.join(process.cwd(), 'lib')
+    mockPackages.forEach((pkg) => {
+        expect(buildPackage).toHaveBeenCalledWith({
+            srcDir: path.join(pkg, 'src'),
+            destDir: path.join(pkg, 'lib')
+        });
     });
 });
--- packages/vitruvius/src/commands/build.js
+++ packages/vitruvius/src/commands/build.js
@@ -1,16 +1,19 @@
 import * as path from 'path';
 import buildPackage from 'vitruvius-build-package';
+import { getPackages } from 'vitruvius-lerna';
 
 export const command = 'build <srcDir> <destDir>';
 
-export const describe = 'build package(s)';
+export const describe = 'build packages';
 
 export const handler = (argv) => {
-    const packageDir = process.cwd();
-    const srcDir = path.join(packageDir, argv.srcDir);
-    const destDir = path.join(packageDir, argv.destDir);
-    buildPackage({
-        srcDir,
-        destDir
+    const packages = getPackages();
+    packages.forEach((pkg) => {
+        const srcDir = path.join(pkg, argv.srcDir);
+        const destDir = path.join(pkg, argv.destDir);
+        buildPackage({
+            srcDir,
+            destDir
+        });
     });
 };
